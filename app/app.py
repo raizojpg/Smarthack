cbb568f5cd14a52a152258093cea83d437154047
AntonioGrigorascu <grigorascu.antonio@gmail.com>
2024-11-02 16:23:30 +0200

    connectionId fix

--- app/app.py
+++ app/app.py
@@ -85,9 +85,10 @@ def main():
     print(session_id)
 
     if session_id:
+        # Example movement
         movements = [
             {
-                "connectionId": session_id,
+                "connectionId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
                 "amount": 0
             }
         ]
